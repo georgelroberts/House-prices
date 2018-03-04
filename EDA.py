2eee0c01b6f89db8ad334b6aaa70bdf25a75a3dc
George <georgelroberts@hotmail.com>
2018-03-04 07:55:45 -0200

    Numeric correlations
    Added correlations to the EDA

--- EDA.py
+++ EDA.py
@@ -17,6 +17,7 @@ Fitting
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import HuberRegressor
+import seaborn as sns
 
 dataFolder = 'C:\\Users\\George\\OneDrive - University Of Cambridge\\Others\\Machine learning\\Kaggle\\House prices\\Data\\'
 
@@ -25,25 +26,44 @@ test = pd.read_csv(dataFolder+'test.csv')
 
 print(train.info())
 
-# Lets first look at the distribution of sale prices. They appear left skewed.
-# This will be easier to see in a box plot.
+#%% Lets first look at the distribution of sale prices. They appear right
+# skewed. This will be easier to see in a box plot.
 
 fig, ax = plt.subplots()
 ax.hist(train.SalePrice, bins=20)
 
-# There are a surprising number of outliers at the expensive range, and there
+#%% There are a surprising number of outliers at the expensive range, and there
 # doesn't appear to be any trend with the year.
 
 train[['SalePrice', 'YrSold']].boxplot(by='YrSold')
 
-# From a human perspective, the lot area looks like the strongest indicator of
-# price, lets see whether a basic fit can be made. Need to use HuberRegressor
-# because it is more robust to outliers
+#%% From a human perspective, the living space looks like the strongest
+# indicator of price, lets see whether a basic fit can be made. Need to use
+# HuberRegressor because it is more robust to outliers.
 
 fig, ax = plt.subplots()
-ax.scatter(train.LotArea, train.SalePrice)
+ax.scatter(train.GrLivArea, train.SalePrice)
 clf = HuberRegressor()
-clf.fit(train.LotArea.reshape(-1, 1), train.SalePrice.reshape(-1, 1))
-salePredictLotArea = clf.predict(train.LotArea.reshape(-1, 1))
-ax.plot(train.LotArea.reshape(-1, 1), clf.predict(train.LotArea.reshape(-1, 1)))
+clf.fit(train.GrLivArea.values.reshape(-1, 1),
+        train.SalePrice.values.reshape(-1, 1))
+salePredictGrLivArea = clf.predict(train.GrLivArea.values.reshape(-1, 1))
+ax.plot(train.GrLivArea.values.reshape(-1, 1),
+        clf.predict(train.GrLivArea.values.reshape(-1, 1)))
 plt.ylim([0, 800000])
+plt.show()
+
+#%% Lets look at correlations in the dataset (at least between numeric values)
+# We only care about correlations with sale price, so lets visualise that.
+# It turns out a number of variables have a large positive correlation
+#  with salePrice, with overall quality ranking higher than living area.
+
+corr = train.corr()
+corr = corr['SalePrice']
+corr.drop('SalePrice', axis=0, inplace=True)
+corr = corr.sort_values(ascending=False)
+fig, ax = plt.subplots()
+corrPlot = sns.barplot(x=corr.index, y=corr.values)
+plt.xticks(rotation=45);
+
+
+
