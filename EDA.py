89f94a84388d20570cac323438a5e2dd1bbd40c3
George <georgelroberts@hotmail.com>
2018-03-09 18:59:42 -0200

    Fit based on all features. Included NaN evaluation in EDA.
    Most of the NAs in the non-numeric features are actually descriptive (see data description and EDA), so these are now included, improving the score.
    
    Also tidied up the graphs in 'EDA' as well as including a section looking at the NaN values.

--- EDA.py
+++ EDA.py
@@ -4,9 +4,6 @@ Created on Sat Mar  3 17:24:32 2018
 
 @author: George
 
-EDA
-TODO: Any correlations between features? (multicollinearity) - maybe use ridge
-         regression
 TODO: Remove missing data and create labels
 TODO: Any significant differences between test and train?
 """
@@ -16,40 +13,51 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import HuberRegressor
 import seaborn as sns
 
-dataFolder = 'C:\\Users\\George\\OneDrive - University Of Cambridge\\Others\\Machine learning\\Kaggle\\House prices\\Data\\'
+dataFolder = 'Data\\'
 
 train = pd.read_csv(dataFolder+'train.csv')
 test = pd.read_csv(dataFolder+'test.csv')
 
 print(train.info())
 
-#%% Lets first look at the distribution of sale prices. They appear right
+# %% Lets first look at the distribution of sale prices. They appear right
 # skewed. This will be easier to see in a box plot.
 
 fig, ax = plt.subplots()
-ax.hist(train.SalePrice, bins=20)
+ax.hist(train.SalePrice/1e3, bins=30, rwidth=0.9)
+plt.xlabel('Sale price (1000$)')
+plt.ylabel('Number of houses')
+plt.show()
 
-#%% There are a surprising number of outliers at the expensive range, and there
+# %% There are a surprising number of outliers at the expensive range, and there
 # doesn't appear to be any trend with the year.
 
-train[['SalePrice', 'YrSold']].boxplot(by='YrSold')
+fig, ax = plt.subplots()
+train[['SalePrice', 'YrSold']].boxplot(by='YrSold', column='SalePrice', ax=ax)
+plt.xlabel('Year sold')
+plt.ylabel('Price ($)')
+plt.suptitle("")
+plt.show()
 
-#%% From a human perspective, the living space looks like the strongest
+# %% From a human perspective, the living space looks like the strongest
 # indicator of price, lets see whether a basic fit can be made. Need to use
 # HuberRegressor because it is more robust to outliers.
 
 fig, ax = plt.subplots()
-ax.scatter(train.GrLivArea, train.SalePrice)
+ax.scatter(train.GrLivArea, train.SalePrice, alpha=0.2, label='Real data')
 clf = HuberRegressor()
 clf.fit(train.GrLivArea.values.reshape(-1, 1),
         train.SalePrice.values.reshape(-1, 1))
 salePredictGrLivArea = clf.predict(train.GrLivArea.values.reshape(-1, 1))
 ax.plot(train.GrLivArea.values.reshape(-1, 1),
-        clf.predict(train.GrLivArea.values.reshape(-1, 1)))
-plt.ylim([0, 800000])
+        clf.predict(train.GrLivArea.values.reshape(-1, 1)),
+        'black', label='Linear fit')
+plt.xlabel('Living area')
+plt.ylabel('Price ($)')
+plt.legend()
 plt.show()
 
-#%% Lets look at correlations in the dataset (at least between numeric values)
+# %% Lets look at correlations in the dataset (at least between numeric values)
 # We only care about correlations with sale price, so lets visualise that.
 # It turns out a number of variables have a large positive correlation
 #  with salePrice, with overall quality ranking higher than living area.
@@ -58,9 +66,68 @@ corr = train.corr()
 corr = corr['SalePrice']
 corr.drop('SalePrice', axis=0, inplace=True)
 corr = corr.sort_values(ascending=False)
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(12,6))
 corrPlot = sns.barplot(x=corr.index, y=corr.values)
 plt.xticks(rotation=45);
+plt.xlabel('House feature')
+plt.ylabel('Correlation')
+plt.show()
 
+# %% We will now decide what to do with columns with null values in. This is
+# done by analysing the data and also looking at the data descriptions. 
+# Actually, XGBoost naturally deals with missing data, so this is unnecessary,
+# at least for this fit. 
 
+nanCols = pd.isnull(train).sum()
+nanCols = nanCols[nanCols>0]
+nanCols = nanCols.sort_values(ascending=False)
 
+print('There are {} features in the training set, {} of which have some null values.'
+      .format(train.shape[1]-2, nanCols.shape[0])) # Subtract label and ID
+
+fig, ax = plt.subplots()
+corrPlot = sns.barplot(x=nanCols.index, y=nanCols.values)
+plt.xticks(rotation=45);
+plt.xlabel('House feature')
+plt.ylabel('Number of Nans')
+plt.show()
+
+# First deal with the object-type NaNs.
+nanObj = train[nanCols.index].select_dtypes(include=['object']).copy()
+# Set modal value for: MasVnrType, Electrical. These have very few missing
+# values
+# Set to zero for: PoolQC, MiscFeature, Alley, Fence, FireplaceQu, GarageType,
+#                  GarageFinish, GarageQual, GarageCond, BsmtFinType2,
+#                  BsmtExposure, BsmtFinType1, BsmtCond, BsmtQual. These Nans
+# are due to the coding used, so they do actually convey information.
+
+modal = ['MasVnrType', 'Electrical']
+for col in modal:
+    train[col].fillna(train[col].mode().values[0], inplace=True)
+    test[col].fillna(train[col].mode().values[0], inplace=True)
+    
+nanObj.drop(modal, axis=1, inplace=True)
+
+for col in nanObj.columns:
+    train[col].fillna(0, inplace=True)
+    test[col].fillna(0, inplace=True)
+
+# nanCols that are numeric. Are the numbers missing for a reason or can we just
+# set the value equal to the average of the whole column? Check for a disparity
+# with salePrice for each.
+
+nanVal = train[nanCols.index].select_dtypes(exclude=['object']).copy()
+
+for col in nanVal.columns:
+    nanColComp = train[[col,'SalePrice']]
+    nullAvg = nanColComp[pd.isnull(nanColComp[col])].SalePrice.mean()
+    otherAvg = nanColComp[pd.notnull(nanColComp[col])].SalePrice.mean()
+    print('For column {}, the average sale price for NaNs is {:.0f}, where for non-NaNs is {:.0f}'
+          .format(col, nullAvg, otherAvg))
+
+# Lets let xgboost deal with all rows where GarageYrBlt and MasVnrArea are
+# NaNs, because it looks like setting it to an average value will skew the
+# results. There aren't many values for these anyway. Lets average LotFrontage
+    
+train['LotFrontage'].fillna(train.LotFrontage.mean(), inplace=True)
+test['LotFrontage'].fillna(train.LotFrontage.mean(), inplace=True)
--- cleanAndFit.py
+++ cleanAndFit.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import LabelEncoder
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 
-dataFolder = 'C:\\Users\\George\\OneDrive - University Of Cambridge\\Others\\Machine learning\\Kaggle\\House prices\\Data\\'
+dataFolder = 'Data\\'
 
 train = pd.read_csv(dataFolder+'train.csv')
 test = pd.read_csv(dataFolder+'test.csv')
@@ -24,31 +24,48 @@ trainX = train.drop('SalePrice', axis=1)
 test = test.set_index('Id')
 trainX = trainX.set_index('Id')
 
-# %% For now lets just drop all columns with NaN values and later I will
-# identify how to deal with each column
-trainSize = trainX.shape[0]
-X = trainX.append(test)
-X = X.dropna(axis=1, how='any')
-trainX = X.loc[0:trainSize]
-test = X.loc[trainSize+1:]
+# %% XGBoost dealds well with NAs, but labelencoder doesn't, so lets deal with
+# the object values (see EDA analysis).
 
-# %% Label encoding for non-numeric data
+#nanCols = pd.isnull(train).sum()
+#nanCols = nanCols[nanCols > 0]
+#nanObj = train[nanCols.index].select_dtypes(include=['object']).copy()
+#modal = ['MasVnrType', 'Electrical']
+#for col in modal:
+#    train[col].fillna(train[col].mode().values[0], inplace=True)
+#    test[col].fillna(train[col].mode().values[0], inplace=True)
+#
+#nanObj.drop(modal, axis=1, inplace=True)
+#
+#for col in nanObj.columns:
+#    train[col].fillna(0, inplace=True)
+#    test[col].fillna(0, inplace=True)
 
 
-def encodeLabels(X):
+# %% Label encoding for non-numeric data. Fit the train and test together so
+# LabelEncoder doesn't throw an error
+
+
+def encodeLabels(XTrain, XTest):
     """Encode non-numeric data types as numeric"""
-    Xobj = X.select_dtypes(include=['object']).copy()
+    trainSize = XTrain.shape[0]
+    X = XTrain.append(XTest)
+
+    Xobj = X.select_dtypes(include=['object']).astype(str).copy()
     Xval = X.select_dtypes(exclude=['object']).copy()
+
     le = LabelEncoder()
     for column in Xobj:
         le.fit(Xobj[column])
         Xobj[column] = le.transform(Xobj[column])
     X = pd.concat([Xval, Xobj], axis=1)
-    return X
+    XTrain = X.loc[0:trainSize]
+    XTest = X.loc[trainSize+1:]
+
+    return XTrain, XTest
 
 
-test = encodeLabels(test)
-trainX = encodeLabels(trainX)
+trainX, test = encodeLabels(trainX, test)
 
 # %% Basic fit on data - Kaggle uses root mean squared log error, so this is
 # implemented and an xgboost regressor is used
@@ -74,4 +91,4 @@ CVError = rmsle(CVY, CVYfit.reshape(-1, 1)).values[0]
 submission = pd.DataFrame(test.index)
 submission['SalePrice'] = model.predict(test)
 subFolder = 'C:\\Users\\George\\OneDrive - University Of Cambridge\\Others\\Machine learning\\Kaggle\\House prices\\Output\\'
-submission.to_csv(subFolder+'XGBoost1.csv', index=False)
+submission.to_csv(subFolder+'XGBoost2.csv', index=False)
